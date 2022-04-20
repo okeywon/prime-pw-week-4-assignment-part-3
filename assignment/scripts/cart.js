8310dce73d43d266eed1135ce5b5ed44958128d6
Courtney Azar <courtney.a.azar@gmail.com>
2022-04-20 11:20:41 -0500

    last stretch goal is kicking my butt, but I'm close

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,14 +2,65 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+let itemName = [];
+const maxItems = 5;
 
 function addItem(itemName) {
-    basket.push(itemName);
-    return basket;
+    if (basket.length <= maxItems){
+        basket.push(itemName);
+        return true;
+    } else {
+        return false;
+    }
 }
 
-console.log(addItem("Potatoes"));
-console.log(addItem("Chips"));
-console.log(addItem("Sausages"));
-console.log(addItem("Nice Crispy Bacon"));
+console.log(addItem('Potatoes'));
+console.log(addItem('Chips'));
+console.log(addItem('Sausages'));
+console.log(addItem('Nice Crispy Bacon'));
+console.log(addItem('Roast veal'));
+console.log(addItem('Meat right off the bone'));
 console.log(basket);
+
+console.log(`Basket is ${basket}`);
+console.log('Trying to add an item to a full basket:', (addItem('Lambas Bread')));
+
+function listItems() {
+    for (let x = 0; x < basket.length; x++) {
+        console.log(basket[x]);
+    }
+}
+
+console.log(listItems());
+
+function empty() {
+    while (basket.length > 0) {
+        basket.splice(0, basket.length);
+        return basket;
+    }
+}
+
+function isFull() {
+    if (basket.length > maxItems) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isFull(basket));
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+
+// This needs more work... I can get it to remove index 1 of basket, just not the correct/exact item.
+function removeItem(array, item) {
+    let index = array.indexOf(item);
+    for (let x = 0; x < basket.length; x++) {
+        if (index > -1 && basket[x] === item); {
+            basket.splice(item, 1);
+            return basket;
+        }
+    }
+}
+console.log(removeItem('Chips'));
