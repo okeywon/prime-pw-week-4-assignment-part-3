65d0700ed2b67db7189d1ec40d352dd290bc5c2d
Courtney Azar <courtney.a.azar@gmail.com>
2022-04-20 10:05:11 -0500

    first function completed

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,15 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(itemName) {
+    basket.push(itemName);
+    return basket;
+}
+
+console.log(addItem("Potatoes"));
+console.log(addItem("Chips"));
+console.log(addItem("Sausages"));
+console.log(addItem("Nice Crispy Bacon"));
+console.log(basket);
